92ae580c4763089ddd99f043b8c2d9c62aa5d3d8
Kano85 <Kano85>
2023-10-11 08:29:30 +0000

    Pushed from Locofy Builder

--- src/pages/Portfolio10092023.vue
+++ src/pages/Portfolio10092023.vue
@@ -4,11 +4,7 @@
   >
     <header navAlignContent="end" />
     <About /><MyRecentWork /><Contact />
-    <footer
-      class="self-stretch bg-base-white overflow-hidden flex flex-col items-start justify-start py-2.5 px-[40.999359130859375px] text-left text-38xl-4 text-primary-main-1 font-roboto-mono"
-    >
-      <b class="self-stretch relative">I can help with that !</b>
-    </footer>
+    <footer />
   </main>
 </template>
 <script>
@@ -17,9 +13,10 @@
   import About from "../components/About.vue";
   import MyRecentWork from "../components/MyRecentWork.vue";
   import Contact from "../components/Contact.vue";
+  import Footer from "../components/Footer.vue";
 
   export default defineComponent({
     name: "Portfolio10092023",
-    components: { Header, About, MyRecentWork, Contact },
+    components: { Header, About, MyRecentWork, Contact, Footer },
   });
 </script>
